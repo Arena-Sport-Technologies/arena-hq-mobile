8615d45b25cd4382c9f612b3ae12d6c84c49b0c6
Edwin Quai Hoi <edwinquaihoi@gmail.com>
2026-03-26 14:53:07 +1100

    centralise firebase flavor loading

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -1,3 +1,11 @@
+import 'dart:developer';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:arenahq_mobile/firebase_options_dev.dart' as dev;
+import 'package:arenahq_mobile/firebase_options_stg.dart' as stg;
+import 'package:arenahq_mobile/firebase_options_prod.dart' as prd;
+
+
 enum Flavor {
   dev,
   stg,
@@ -10,6 +18,9 @@ class F {
   static String get name => appFlavor.name;
 
   static String get title {
+
+    log('Get title  for $appFlavor');
+
     switch (appFlavor) {
       case Flavor.dev:
         return 'Arena HQ Development';
@@ -17,6 +28,20 @@ class F {
         return 'Arena HQ Staging';
       case Flavor.prod:
         return 'Arena HQ Production';
+    }
+  }
+
+  static FirebaseOptions get firebaseOptions {
+
+    log('Get Firebase options for $appFlavor');
+
+    switch (appFlavor) {
+      case Flavor.dev:
+        return dev.DefaultFirebaseOptions.currentPlatform;
+      case Flavor.stg:
+        return stg.DefaultFirebaseOptions.currentPlatform;
+      case Flavor.prod:
+        return prd.DefaultFirebaseOptions.currentPlatform;
     }
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,41 @@
 import 'dart:async';
+import 'dart:developer';
+import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'app.dart';
 import 'flavors.dart';
 
-void main() {
+void main() async {
+
+  log('Flavor $appFlavor');
+
   F.appFlavor = Flavor.values.firstWhere(
     (element) => element.name == appFlavor,
   );
 
+  
+  log("Initialising Firebase");
+  FirebaseOptions fbOpt = F.firebaseOptions;
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  log('Got Firebase Options for ${fbOpt.appId}');
+  await Firebase.initializeApp(options: F.firebaseOptions);
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  log('Firebase initialised!');
   runApp(const App());
 }
